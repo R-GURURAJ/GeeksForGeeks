f1032043b5fe564714e34a92d4a804fa7bf6a742
R-GURURAJ <guru1guru0@gmail.com>
2023-08-09 18:09:49 +0530

    public

---
+++ python/TrappingRainWater.py
@@ -0,0 +1,20 @@
+class Solution:
+    def trappingWater(self, arr,n):
+        s=0
+        rigMax = max(arr[2:])
+        lefMax = arr[0]
+        for i in range(1,n-1):
+            if arr[i]>lefMax:
+                lefMax = arr[i]
+            
+            if arr[i] == rigMax :
+                rigMax = max(arr[i+1:])
+            if lefMax<rigMax:
+                mini = lefMax
+            else:
+                mini = rigMax
+            
+            diff = mini -arr[i]
+            if diff>0:
+                s+=diff
+        return s
